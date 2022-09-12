fba86813e9ab3f0d5fb1b9e0eaee1f1f7a45785a
emjay0921 <michaelgonzales2117@gmail.com>
2022-09-12 17:49:11 +0800

    add field on g2p_registry_rest_api_extension_demo

--- g2p_registry_rest_api_extension_demo/models/group.py
+++ g2p_registry_rest_api_extension_demo/models/group.py
@@ -1,5 +1,7 @@
+import pydantic
+
 from odoo.addons.g2p_registry_rest_api.models.group import GroupInfoOut
 
 
 class GroupInfoOutExtended(GroupInfoOut, extends=GroupInfoOut):
-    z_ind_grp_num_children: int
+    area: int = pydantic.Field(..., alias="area_id")
