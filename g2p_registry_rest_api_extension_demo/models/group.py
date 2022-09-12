e5de4b4f8c0a0aed8b9ca9fbdd10eebb6716fc7a
emjay0921 <michaelgonzales2117@gmail.com>
2022-09-12 18:22:39 +0800

    change area to active field

--- g2p_registry_rest_api_extension_demo/models/group.py
+++ g2p_registry_rest_api_extension_demo/models/group.py
@@ -4,4 +4,4 @@ from odoo.addons.g2p_registry_rest_api.models.group import GroupInfoOut
 
 
 class GroupInfoOutExtended(GroupInfoOut, extends=GroupInfoOut):
-    area: int = pydantic.Field(..., alias="area_id")
+    active: bool
