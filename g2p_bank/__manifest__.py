b7a69310b31d912ba45fb52ec9922a97e93f2c08
emjay0921 <michaelgonzales2117@gmail.com>
2022-09-14 16:33:56 +0800

    add bank module

---
+++ g2p_bank/__manifest__.py
@@ -0,0 +1,30 @@
+# Part of OpenG2P Registry. See LICENSE file for full copyright and licensing details.
+{
+    "name": "G2P Registry: Bank Details",
+    "category": "G2P",
+    "version": "15.0.0.0.1",
+    "sequence": 1,
+    "author": "OpenG2P",
+    "website": "https://github.com/openg2p/openg2p-registry",
+    "license": "Other OSI approved licence",
+    "development_status": "Production/Stable",
+    "maintainers": ["jeremi", "gonzalesedwin1123", "emjay0921"],
+    "depends": [
+        "base",
+        "mail",
+        "contacts",
+        "g2p_registry_group",
+        "g2p_registry_individual",
+    ],
+    "external_dependencies": {"python": ["schwifty"]},
+    "data": [
+        "views/individuals_view.xml",
+        "views/groups_view.xml",
+    ],
+    "assets": {},
+    "demo": [],
+    "images": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
