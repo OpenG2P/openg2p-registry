3ef5f7b2ba1d7d1e9debaa80df7a33a5137c002e
Edwin Gonzales <edwin@newlogic.com>
2022-11-02 06:40:39 +0800

    Change JWT module dependency to spp_rest_auth_jwt

--- g2p_rest_auth_jwt/components/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of OpenG2P Registry. See LICENSE file for full copyright and licensing details.
-from . import auth_jwt_component_context_provider
-from . import service
--- g2p_rest_auth_jwt/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of OpenG2P Registry. See LICENSE file for full copyright and licensing details.
-from . import auth_jwt_validator
-from . import ir_http
