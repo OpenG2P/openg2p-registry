3fe5f7432e6ebbeb5944ca8fae2316a4e8e40d8f
Shibu Narayanan <104964065+shibu-narayanan@users.noreply.github.com>
2023-03-07 23:51:56 +0530

    dict to list for additional info

--- g2p_registry_addl_info_rest_api/models/registrant.py
+++ g2p_registry_addl_info_rest_api/models/registrant.py
@@ -4,10 +4,10 @@ from odoo.addons.g2p_registry_rest_api.models import registrant
 class RegistrantAddlInfoIn(
     registrant.RegistrantInfoIn, extends=registrant.RegistrantInfoIn
 ):
-    additional_g2p_info: dict = {}
+    additional_g2p_info: list = []
 
 
 class RegistrantAddlInfoOut(
     registrant.RegistrantInfoOut, extends=registrant.RegistrantInfoOut
 ):
-    additional_g2p_info: dict = {}
+    additional_g2p_info: list = []
