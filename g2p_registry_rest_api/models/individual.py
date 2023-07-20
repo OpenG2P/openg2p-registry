74316943c9f0ba0559f64dfdb556f66a3a969307
Abhishek Wagh <abhishek.created20@gmail.com>
2023-07-20 15:09:20 +0530

    Added validator for given name and family name in post call for creating individual record

--- g2p_registry_rest_api/models/individual.py
+++ g2p_registry_rest_api/models/individual.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+from pydantic import validator
+
 from .registrant import RegistrantInfoIn, RegistrantInfoOut
 
 
@@ -15,10 +17,23 @@ class IndividualInfoOut(RegistrantInfoOut):
 
 
 class IndividualInfoIn(RegistrantInfoIn):
-    given_name: str = None
+    given_name: str
     addl_name: str = None
-    family_name: str = None
+    family_name: str
+    gender: str = None
     gender: str = None
     birthdate: date = None
     birth_place: str = None
     is_group = False
+
+    @validator("given_name")
+    def validate_given_name(cls, v):
+        if v is None or v.strip() == "":
+            raise ValueError("Given name is mandatory.")
+        return v
+
+    @validator("family_name")
+    def validate_family_name(cls, v):
+        if v is None or v.strip() == "":
+            raise ValueError("Family name is mandatory.")
+        return v
