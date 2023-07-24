8ef6d653b627a8a5dd3058b590b6b71452aec685
Abhishek Wagh <abhishek.created20@gmail.com>
2023-07-24 11:02:12 +0530

    added validation for memberships kind type

--- g2p_registry_rest_api/models/group_membership.py
+++ g2p_registry_rest_api/models/group_membership.py
@@ -1,6 +1,8 @@
 from datetime import date
 from typing import List, Optional
 
+from pydantic import validator
+
 from .individual import IndividualInfoOut
 from .naive_orm_model import NaiveOrmModel
 from .registrant import PhoneNumberIn, RegistrantIDIn
@@ -8,6 +10,17 @@ from .registrant import PhoneNumberIn, RegistrantIDIn
 
 class GroupMembershipKindInfo(NaiveOrmModel):
     name: str
+
+    @validator("name")
+    def validate_name_no_spaces(cls, value):
+        # Using lstrip() to remove leading spaces from the value
+        value = value.lstrip() if value else value
+
+        # Checking if the length of the cleaned value is less than 1
+        if len(value) < 1:
+            raise ValueError("Name cannot be empty or contain only spaces.")
+
+        return value
 
 
 class GroupMembersInfoOut(NaiveOrmModel):
