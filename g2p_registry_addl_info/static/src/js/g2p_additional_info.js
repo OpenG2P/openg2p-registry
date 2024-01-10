e68e2df233128446fc42bf1a0eef49ef1adf4218
Dibik <dibik@openg2p.org>
2024-01-11 04:14:52 +0530

    G2P-1692: Json widget migrated to odoo17

--- g2p_registry_addl_info/static/src/js/g2p_additional_info.js
+++ g2p_registry_addl_info/static/src/js/g2p_additional_info.js
@@ -1,57 +1,76 @@
 /** @odoo-module **/
 
-import {textField} from "@web/views/fields/text/text_field";
+import {_t} from "@web/core/l10n/translation";
 import {registry} from "@web/core/registry";
-import {qweb} from "web.core";
+import {TextField} from "@web/views/fields/text/text_field";
+import {useState} from "@odoo/owl";
+import {useService} from "@web/core/utils/hooks";
 
-// TODO: Have to changes this widget to odoo17 verison
-var G2PRegAdditionalInfoWidget = textField.extend({
-    className: "o_field_g2p_addl_info",
-    supportedFieldTypes: ["json", "text", "html"],
-    tagName: "div",
-    _renderReadonly: function () {
+export class JsonFieldWidgets extends TextField {
+
+    setup() {
+        super.setup();
+        this.state = useState({recordClicked: false, issueRaised: false});
+        this.notification = useService("notification");
+    }
+
+    onclick(event) {
+        if (this.editingRecord && event) {
+            this.state.recordClicked = true;
+        } else {
+            this.state.recordClicked = false;
+        }
+    }
+
+    get editingRecord() {
+        return !this.props.readonly;
+    }
+
+    get renderjson() {
         try {
-            let valuesJsonOrig = this.value;
+            let valuesJsonOrig = this.props.record.data.additional_g2p_info;
             if (typeof valuesJsonOrig === "string" || valuesJsonOrig instanceof String) {
-                valuesJsonOrig = JSON.parse(this.value);
+                valuesJsonOrig = JSON?.parse(valuesJsonOrig);
             }
 
             if (Array.isArray(valuesJsonOrig)) {
                 const sectionsJson = {};
-                var self = this;
                 valuesJsonOrig.forEach((element) => {
-                    sectionsJson[element.name] = self.flattenJson(element.data);
+                    sectionsJson[element.name] = this.flattenJson(element.data);
                 });
-                return this.$el.html(
-                    qweb.render("addl_info_template", {
-                        sections: sectionsJson,
-                    })
-                );
-            }
 
-            const valuesJson = this.flattenJson(valuesJsonOrig);
-            return this.$el.html(
-                qweb.render("addl_info_each_table", {
-                    flatJson: valuesJson,
-                })
-            );
+                return sectionsJson;
+            } else {
+                const valuesJson = this.flattenJson(valuesJsonOrig);
+                return valuesJson;
+            }
         } catch (err) {
-            console.error(err);
+            this.notification.add(_t("Additional Information"), {
+                title: _t("Invalid Json Value"),
+                type: "danger",
+                sticky: true,
+            });
+            this.state.recordClicked = true;
+            this.state.issueRaised = true;
+            return {};
         }
-        return this._super();
-    },
-    flattenJson: function (object) {
-        const jsonObject = JSON.parse(JSON.stringify(object));
+    }
+
+    flattenJson(object) {
+        const jsonObject = JSON?.parse(JSON?.stringify(object));
         for (const key in jsonObject) {
             if (typeof jsonObject[key] === "object") {
-                jsonObject[key] = JSON.stringify(jsonObject[key]);
+                jsonObject[key] = JSON?.stringify(jsonObject[key]);
             }
         }
         return jsonObject;
-    },
-    _isSameValue: function (value) {
-        return _.isEqual(value, this.value);
-    },
-});
+    }
+}
+JsonFieldWidgets.template = addl_info_each_table;
 
-registry.category("fields").add("g2p_registry_addl_info_widget", G2PRegAdditionalInfoWidget);
+export const jsonFieldWidgets = {
+    component: JsonFieldWidgets,
+    supportedTypes: ["json", "text", "html"],
+};
+
+registry.category("fields").add("g2p_registry_addl_info_widget", jsonFieldWidgets);
