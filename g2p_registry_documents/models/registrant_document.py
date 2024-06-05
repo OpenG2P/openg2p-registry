102cf6a9583a445fc5529e3126465e7bf628ca76
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2024-06-05 14:36:40 +0300

    Implement Registry Document

---
+++ g2p_registry_documents/models/registrant_document.py
@@ -0,0 +1,6 @@
+from odoo import fields, models, api
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    supporting_documents_ids = fields.One2many("storage.file", "registrant_id")
---
+++ g2p_registry_documents/static/src/js/preview_document.js
@@ -0,0 +1,64 @@
+/** @odoo-module **/
+import {Component, xml} from "@odoo/owl";
+import {registry} from "@web/core/registry";
+import {useFileViewer} from "@web/core/file_viewer/file_viewer_hook";
+import {useService} from "@web/core/utils/hooks";
+
+class Widgetpreview extends Component {
+    static template = xml`<button class="btn btn-primary" icon="fa-file-text-o" t-on-click="clickPreview">Preview</button>`;
+
+    setup() {
+        super.setup();
+        this.fileViewer = useFileViewer();
+        this.store = useService("mail.store");
+        this.rpc = useService("rpc");
+    }
+
+    clickPreview(ev) {
+        const currentRow = ev.target.closest(".o_data_row");
+        if (currentRow) {
+            const slugElement = currentRow.querySelector('.o_data_cell[name="slug"]');
+            if (slugElement) {
+                const slugValue = slugElement.textContent.trim();
+                console.log("Slug Value:", slugValue);
+
+                let recordID = 0;
+                if (slugValue.includes("-")) {
+                    const parts = slugValue.split("-");
+                    const lastPart = parts[parts.length - 1].split(".")[0];
+                    if (!isNaN(lastPart)) {
+                        recordID = parseInt(lastPart, 10);
+                    }
+                }
+                if (recordID) {
+                    this._onPreviewButtonClick(recordID);
+                }
+            }
+        }
+    }
+
+    async _onPreviewButtonClick(recordID) {
+        const result = await this.rpc("/web/dataset/call_kw/storage.file/get_binary", {
+            model: "storage.file",
+            method: "get_binary",
+            args: [[recordID]],
+            kwargs: {},
+        });
+        const attach_id = parseInt(result.id, 10);
+        const mimetype = result.mimetype;
+        const indexContent = result.index_content || "";
+        window.open(result.url, "_blank");
+        if (mimetype.includes("image")) {
+            const attachment = this.store.Attachment.insert({
+                id: attach_id,
+                mimetype: mimetype,
+                fileType: indexContent,
+            });
+            this.fileViewer.open(attachment);
+        } else {
+            window.open(result.url, "_blank");
+        }
+    }
+}
+
+registry.category("view_widgets").add("action_preview", {component: Widgetpreview});
