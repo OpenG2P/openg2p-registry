3d505facc42677833344c62eee81a5b5e4aafedd
Lalith Kota <kotalalith@gmail.com>
2025-04-30 11:27:10 +0530

    Fixed documents storage file api access
    Signed-off-by: Lalith Kota <kotalalith@gmail.com>

--- g2p_documents/controllers/storage_file.py
+++ g2p_documents/controllers/storage_file.py
@@ -1,16 +1,23 @@
-from odoo.http import request
+from werkzeug.exceptions import Forbidden
+
+from odoo.http import request, route
 
 from odoo.addons.storage_file.controllers.main import StorageFileController
 
 
 class StorageFileControllerExt(StorageFileController):
+    @route(["/storage.file/<string:slug_name_with_id>"], type="http", auth="public")
     def content_common(self, *args, **kw):
-        storage_file = (
+        storage_file_api_enabled = (
             request.env["ir.config_parameter"]
             .sudo()
-            .get_param("g2p_documents.enable_storage_file_api", "False")
-            == "True"
+            .get_param("g2p_documents.enable_storage_file_api", "user")
         )
-        if not storage_file:
-            return request.not_found()
+        if storage_file_api_enabled == "user":
+            if (not request.env.user) or (not request.env.user.has_group("base.group_user")):
+                raise Forbidden("You don't have the permission to access the requested resource.")
+        elif storage_file_api_enabled == "public":
+            pass
+        else:
+            raise request.not_found()
         return super().content_common(*args, **kw)
