80da2844b29da2cee3636f97469481ff632c4536
Manoj Kumar <mkumar6@ch.iitr.ac.in>
2025-05-09 12:34:07 +0530

    Registration Portal: Update license
    Signed-off-by: Manoj Kumar <mkumar6@ch.iitr.ac.in>

--- g2p_registration_portal_base/__manifest__.py
+++ g2p_registration_portal_base/__manifest__.py
@@ -1,11 +1,11 @@
 {
-    "name": "G2P Registration Portal :Base",
+    "name": "G2P Registration Portal: Base",
     "category": "G2P",
     "version": "17.0.1.5.0",
     "sequence": 1,
     "author": "OpenG2P",
     "website": "https://openg2p.org",
-    "license": "Other OSI approved licence",
+    "license": "LGPL-3",
     "depends": [
         "g2p_agent_portal_base",
         "g2p_registry_membership",
