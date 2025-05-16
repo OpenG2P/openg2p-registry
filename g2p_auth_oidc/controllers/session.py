bef717820228a2fcf7435b9606c549ad29a1dcf3
vin0dkhichar <abbb61667@gmail.com>
2025-05-16 17:31:23 +0530

    Added session logout for auth oauth provider

---
+++ g2p_auth_oidc/controllers/session.py
@@ -0,0 +1,22 @@
+from werkzeug.utils import redirect as werkzeug_redirect
+
+from odoo import http
+from odoo.http import request
+
+from odoo.addons.web.controllers.session import Session
+
+
+class OAuthLogoutController(Session):
+    @http.route("/web/session/logout", type="http", auth="user")
+    def logout(self, redirect="/web"):
+        user = request.env.user
+        provider = user.oauth_provider_id
+
+        request.session.logout()
+
+        # Redirect to OAuth provider logout URL if configured
+        if provider and provider.logout_uri:
+            redirect = provider.logout_uri
+            return werkzeug_redirect(redirect, 302)
+
+        return super().logout(redirect=redirect)
