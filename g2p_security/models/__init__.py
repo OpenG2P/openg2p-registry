ccca527a097ebf0aa865bf3ac377fd7928663540
Niranjan Kumar <niranjan882684@gmail.com>
2025-05-21 22:51:47 +0530

    Improved user session and reverted file to previous state

--- g2p_security/models/__init__.py
+++ g2p_security/models/__init__.py
@@ -1,6 +1,5 @@
 # Part of OpenG2P. See LICENSE file for full copyright and licensing details.
 
 from . import hide_db_param
-from . import ir_config_parameter
-from . import ir_http
-from . import res_users
+from . import user_debug_restriction
+from . import user_session
