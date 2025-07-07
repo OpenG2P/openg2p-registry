65fdaf951b33ce82b51d17e2c50808760b0c45e4
vin0dkhichar <abbb61667@gmail.com>
2025-07-07 14:33:42 +0530

    Add change password wizard action on res.partner

--- g2p_agent_portal_base/__manifest__.py
+++ g2p_agent_portal_base/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "OpenG2P",
     "website": "https://openg2p.org",
     "license": "LGPL-3",
-    "depends": ["account"],
+    "depends": ["account", "g2p_registry_base"],
     "data": [
         "views/about_us.xml",
         "views/base.xml",
--- g2p_security/models/hide_db_param.py
+++ g2p_security/models/hide_db_param.py
@@ -1,6 +1,7 @@
 import werkzeug.urls
 
-from odoo import models
+from odoo import _, models
+from odoo.exceptions import UserError
 
 
 class ResPartner(models.Model):
@@ -21,3 +22,37 @@ class ResPartner(models.Model):
                     cleaned_url = parsed.replace(query=werkzeug.urls.url_encode(query)).to_url()
                     res[pid] = cleaned_url
         return res
+
+    def action_change_password(self):
+        """Open change password wizard for partner's user account"""
+        if not self.user_ids:
+            raise UserError(_("No user account found for this partner"))
+
+        user = self.user_ids[0]
+
+        wizard = self.env["change.password.wizard"].create(
+            {
+                "user_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "user_id": user.id,
+                            "user_login": user.login,
+                            "new_passwd": "",
+                        },
+                    )
+                ]
+            }
+        )
+
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Change Password"),
+            "res_model": "change.password.wizard",
+            "res_id": wizard.id,
+            "view_mode": "form",
+            "view_id": self.env.ref("base.change_password_wizard_view").id,
+            "target": "new",
+            "context": {"default_user_ids": wizard.user_ids.ids},
+        }
